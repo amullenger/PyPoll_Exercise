5019ab31144d6bab21c2f59c398c97e029b24d4a
Andrew Mullenger <amullenger@wustl.edu>
2019-02-05 23:40:36 -0600

    final commented version of main2.py

--- main2.py
+++ main2.py
@@ -3,6 +3,9 @@ import csv
 
 election_data_path = os.path.join('', 'election_data.csv')
 
+"""Instantiate variables for storing all data in a master list, tracking total votes and 
+individual vote counts, storing results, and calculating the winner"""
+
 master_list = []
 total_votes = 0
 num_khan_votes = 0
@@ -12,6 +15,9 @@ num_otooley_votes = 0
 results = []
 winner = ''
 
+"""Open file, store data in reader object, and append each row to a master list so we can 
+easily perform necessary operations. Using next(reader object) function will skip the header 
+row so our master list only contains voting data"""
 
 with open(election_data_path, 'r', newline='') as csvfile:
     election_data_reader = csv.reader(csvfile)
@@ -19,10 +25,12 @@ with open(election_data_path, 'r', newline='') as csvfile:
 
     for row in election_data_reader:
         master_list.append([float(row[0]), row[1], row[2]])
-for i in range(5):
-    print(master_list[i])
+
+#The total # of votes is simply the length of the master list
+
 total_votes=len(master_list)
-print(total_votes)
+
+#Iterate through master list and update vote total variables 
 
 for i in range(len(master_list)):
     if master_list[i][2] == 'Khan':
@@ -34,6 +42,8 @@ for i in range(len(master_list)):
     if master_list[i][2] == "O'Tooley":
         num_otooley_votes += 1
 
+#Determine winner by storing candidate name with highest # of votes
+
 results_tracker = 0
 results = [['Khan', num_khan_votes], ['Correy', num_correy_votes], ['Li', num_li_votes], ["O'Tooley", num_otooley_votes]]
 for i in range(len(results)):
@@ -41,7 +51,7 @@ for i in range(len(results)):
         results_tracker = results[i][1]
         winner = results[i][0]
 
-print(num_khan_votes)
+#print results in desired format
 
 print(f"""\
     Election Results
